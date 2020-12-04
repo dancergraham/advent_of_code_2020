cbf4b091818c752c28e2a845af88464944fb9b42
dancergraham <graham.knapp@gmail.com>
2020-12-04 23:15:28 +0100

    Solutions to day 1

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from datetime import date
 
+def day1():
+    with open(r'input/day1.txt') as f:
+        numbers = sorted(int(x) for x in f.readlines())
+    for i, num1 in enumerate( numbers):
+        for j, num2 in enumerate(numbers[i+1:]):
+            if (num1 + num2) == 2020:
+                part_1 = num1 * num2
+            for num3 in numbers[i+j:]:
+                if (num1 + num2 + num3) == 2020:
+                    part_2 = num1 * num2 * num3
+                    break
+    return part_1, part_2
+
+
+
 def day2():
     with open('input/day2.txt') as f:
         passwords = f.readlines()
@@ -17,7 +32,7 @@ def day2():
     return part_1, part_2
     
 
-def aocday3():
+def day3():
     with open('input/day3.txt') as f:
         input_data = [list(line.strip()) for line in f.readlines()]
     product =1
@@ -43,6 +58,6 @@ if __name__ == '__main__':
     today = date.today()
     if today.month == 12:
         print('Merry Christmas')
-    print(day2())
+    print(day1())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
